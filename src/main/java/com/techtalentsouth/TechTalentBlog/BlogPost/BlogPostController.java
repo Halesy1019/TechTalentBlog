0752a07f9cc5f7e17c5ea2dc966125b55087612a
Joshua Luppes <cooljoebob64@gmail.com>
2022-12-28 12:35:04 -0600

    Fix endpoint for editing entry

--- src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -63,7 +63,7 @@ public class BlogPostController {
 
 	}
 
-	@RequestMapping(value = "/blogposts/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/blogposts/update/{id}", method = RequestMethod.GET)
 	public String editPostWithId(@PathVariable Long id, BlogPost blogPost, Model model) {
 		Optional<BlogPost> post = blogPostRepository.findById(id);
 		if (post.isPresent()) {
@@ -73,7 +73,19 @@ public class BlogPostController {
 		return "blogpost/edit.html";
 	}
 
-	@RequestMapping(value = "/blogposts/update/{id}")
+	// @RequestMapping(value = "/blogposts/update/{id}")
+	// public String editPostById(@PathVariable Long id, BlogPost blogPost, Model
+	// model){
+	// Optional<BlogPost> post = blogPostRepository.findById(id);
+	// if (post.isPresent()) {
+
+	// return "blogpost/edit.html";
+	// }
+	// return "blogpost/index.html";
+
+	// }
+
+	@PostMapping(value = "/blogposts/update/{id}")
 	public String updateExistingPost(@PathVariable Long id, BlogPost blogPost, Model model) {
 		Optional<BlogPost> post = blogPostRepository.findById(id);
 		if (post.isPresent()) {
@@ -90,8 +102,8 @@ public class BlogPostController {
 
 	// @RequestMapping(value = "/blogposts/delete/{id}")
 	// public String deletePostById(@PathVariable Long id, BlogPost blogPost) {
-	// 	blogPostRepository.deleteById(id);
-	// 	return "blogpost/delete.html";
+	// blogPostRepository.deleteById(id);
+	// return "blogpost/delete.html";
 	// }
 
 }
